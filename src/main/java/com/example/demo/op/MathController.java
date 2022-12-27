791fd355816d8a53872c7c6e0e670341c4c65cc3
Preisler25 <preisler.andras@gmail.com>
2022-12-28 00:43:05 +0100

    Html to Db working

--- src/main/java/com/example/demo/op/MathController.java
+++ src/main/java/com/example/demo/op/MathController.java
@@ -3,9 +3,7 @@ package com.example.demo.op;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MathController {
@@ -17,27 +15,29 @@ public class MathController {
         return "temp/index";
     }
 
-    @RequestMapping("doMath")
-    public String doMath(@RequestParam String num1, @RequestParam String num2, @RequestParam String op) {
-
+    @RequestMapping(method = RequestMethod.POST, value="/math")
+    public void doMath(@RequestBody String temp) {
+        String[] arrOfStr = temp.split("&", 3);
+        int num1 = Integer.parseInt(arrOfStr[0].split("=")[1]);
+        int num2 = Integer.parseInt(arrOfStr[1].split("=")[1]);
+        String op = arrOfStr[2].split("=")[1];
         switch (op) {
             case "add":
-                System.out.println("A Számok összege " + (Integer.parseInt(num1) + Integer.parseInt(num2)));
-                opRepository.save(new Operations(Integer.parseInt(num1), Integer.parseInt(num2), "add"));
-                return "temp/index";
+                System.out.println("A Számok összege " + (num1 + num2));
+                opRepository.save(new Operations(num1, num2, op));
+                break;
             case "sub":
-                System.out.println("A Számok különbsége " + (Integer.parseInt(num1) - Integer.parseInt(num2)));
-                opRepository.save(new Operations(Integer.parseInt(num1), Integer.parseInt(num2), "sub"));
-                return "temp/index";
+                System.out.println("A Számok különbsége " + (num1 - num2));
+                opRepository.save(new Operations(num1, num2, op));
+                break;
             case "mul":
-                System.out.println("A Számok szorzata " + (Integer.parseInt(num1) * Integer.parseInt(num2)));
-                opRepository.save(new Operations(Integer.parseInt(num1), Integer.parseInt(num2), "mul"));
-                return "temp/index";
+                System.out.println("A Számok szorzata " + (num1 * num2));
+                opRepository.save(new Operations(num1, num2, op));
+                break;
             case "div":
-                System.out.println("A Számok hányadosa " + (Integer.parseInt(num1) / Integer.parseInt(num2)));
-                opRepository.save(new Operations(Integer.parseInt(num1), Integer.parseInt(num2), "div"));
-                return "temp/index";
+                System.out.println("A Számok hányadosa " + (num1) / num2);
+                opRepository.save(new Operations(num1, num2, op));
+                break;
         }
-        return "/math";
     }
 }
