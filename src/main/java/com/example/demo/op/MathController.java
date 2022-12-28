84bd7d2d03eb42e85707db3308b2183c3f715ea4
Preisler25 <preisler.andras@gmail.com>
2022-12-29 00:59:59 +0100

    c code

--- src/main/java/com/example/demo/op/MathController.java
+++ src/main/java/com/example/demo/op/MathController.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class MathController {
@@ -56,4 +56,3 @@ public class MathController {
     }
 
 }
-
